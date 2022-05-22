cef212567e2f33a496b4f266c43123036fe82b3a
kitsunnet | Мацко Егор <76002087+fuett966@users.noreply.github.com>
2022-05-22 20:16:18 +0300

    realese

--- Assets/BossFight.cs
+++ Assets/BossFight.cs
@@ -34,10 +34,10 @@ public class BossFight : MonoBehaviour
         source = gameObject.GetComponent<AudioSource>();
         source.clip = audio;
 
-        text.text = "Ответьте на вопрос, встав на платформу: Сколько будет 2 + 2 ? ";
-        redTEXT.text = "4";
-        blueTEXT.text = "6";
-        greenTEXT.text = "5";
+        text.text = "Разгадай загадку, встав на платформу: Ты творец пшеницы и ячменя.Твои воды дают жизнь всему живому";
+        redTEXT.text = "Нил";
+        blueTEXT.text = "Асирис";
+        greenTEXT.text = "Кратус";
         questionNumber = 0;
     }
 
@@ -48,10 +48,10 @@ public class BossFight : MonoBehaviour
             case 0:
                 if (redPlatform.GetComponent<BossPlatformScript>().isHere)
                 {
-                    text.text = "Ответьте на вопрос, встав на платформу: Сколько лет на земле существует смерть ? ";
-                    redTEXT.text = "0";
-                    blueTEXT.text = "Всё время";
-                    greenTEXT.text = "Неизвестно";
+                    text.text = "Священное животное богини Баст ? ";
+                    redTEXT.text = "Ястреб";
+                    blueTEXT.text = "Крокодил";
+                    greenTEXT.text = "Кошка";
                     questionNumber = 1;
                 }
                 else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
@@ -73,10 +73,10 @@ public class BossFight : MonoBehaviour
                 }
                 else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
                 {
-                    text.text = "Ответьте на вопрос, встав на платформу: Из-за чего горит Солнце? ";
-                    redTEXT.text = "Неизвестно";
-                    greenTEXT.text = "Фотосинтез";
-                    blueTEXT.text = "Горение";
+                    text.text = "Высшее существо, озаряющее нас каждый день ";
+                    redTEXT.text = "Аракс";
+                    greenTEXT.text = "Гепосис";
+                    blueTEXT.text = "Амон-Ра";
                     questionNumber = 2;
                     
                 }
@@ -99,11 +99,172 @@ public class BossFight : MonoBehaviour
                 }
                 else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
                 {
-                    text.text = "Поздравляю - это победа... ";
-                    redTEXT.text = "";
-                    blueTEXT.text = "";
-                    greenTEXT.text = "";
+                    text.text = "Надену весной свой лучший наряд, зимою лишусь покрова;нагой простою - пусть другие дрожат, а к лету закутаюсь снова.";
+                    redTEXT.text = "Земля";
+                    blueTEXT.text = "Дерево";
+                    greenTEXT.text = "Заяц";
                     questionNumber = 3;
+                }
+                break;
+            case 3:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Кто не дышит, но живёт;хоть не нужно - много пьет;и в жизни, и в смерти тело как лед";
+                    redTEXT.text = "Рыба";
+                    blueTEXT.text = "Червь";
+                    greenTEXT.text = "Змея";
+                    questionNumber = 4;
+                }
+                break;
+            case 4:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Ввысь идет, но не растет.На деревья сверху взирает, но корни свои скрывает";
+                    redTEXT.text = "Солнце";
+                    blueTEXT.text = "Человек";
+                    greenTEXT.text = "Гора";
+                    questionNumber = 5;
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                break;
+            case 5:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Ночью на месте без лишних слов, к утру же очищен небесный покров.";
+                    redTEXT.text = "Звёзды";
+                    greenTEXT.text = "Луна";
+                    blueTEXT.text = "Облака";
+                    questionNumber = 6;
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                break;
+            case 6:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Мертвых оживляет. Нас смешит,а порой - печалит.Рождается виг. Гибнет, когда ты - старик.";
+                    redTEXT.text = "Смерть";
+                    blueTEXT.text = "Память";
+                    greenTEXT.text = "Памятник";
+                    questionNumber = 7;
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                break;
+            case 7:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Мала как песчинка, лечу как пушинка.Сначала услышишь, потом лишь заметишь.";
+                    redTEXT.text = "Колибри";
+                    blueTEXT.text = "Стрела";
+                    greenTEXT.text = "Капля";
+                    questionNumber = 8;
+                }
+                break;
+            case 8:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Я жертву мягкой нитью обовью, а после обниму и отравлю.";
+                    redTEXT.text = "Паук";
+                    blueTEXT.text = "Искушение";
+                    greenTEXT.text = "Медоед";
+                    questionNumber = 9;
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                break;
+            case 9:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Вдаль спешу, но не бегу. Всё шумлю, сама молчу. Бьюсь о край - слез не видать. Не проси , не двинусь вспять.";
+                    redTEXT.text = "Птица";
+                    blueTEXT.text = "Облако";
+                    greenTEXT.text = "Река";
+                    questionNumber = 10;
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                break;
+            case 10:
+                if (redPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
+                }
+                else if (greenPlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    text.text = "Поздравляю с победой...";
+                    redTEXT.text = "";
+                    greenTEXT.text = "";
+                    blueTEXT.text = "";
+                    questionNumber = 11;
+
+                }
+                else if (bluePlatform.GetComponent<BossPlatformScript>().isHere)
+                {
+                    GameManager.game.lose.enabled = true;
+                    StartCoroutine(WaitToDeath());
                 }
                 break;
         }
